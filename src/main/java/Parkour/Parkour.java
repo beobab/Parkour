f0b1fc67da58a04972ae11baf4a9eda3b02d0593
beobab <github@toolan.net>
2017-04-01 19:22:35 +0100

    Register race command.

--- src/main/java/Parkour/Parkour.java
+++ src/main/java/Parkour/Parkour.java
@@ -1,4 +1,5 @@
 package Parkour;
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Created by jonathan on 01/04/2017.
@@ -10,9 +11,15 @@ package Parkour;
  * race end
  *   Next plate you go over will mark the END of the race. The finish line.
  *
- */
-
-import org.bukkit.plugin.java.JavaPlugin;
-
+ **/
 public class Parkour extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
 }
