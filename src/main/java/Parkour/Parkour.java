a6c032cb695cefb6a7fc4caf9ad92ae91c761fee
beobab <github@toolan.net>
2017-04-01 19:02:11 +0100

    Empty Plugin

---
+++ src/main/java/Parkour/Parkour.java
@@ -0,0 +1,18 @@
+package Parkour;
+
+/**
+ * Created by jonathan on 01/04/2017.
+ * race start name
+ *   First plate you go over will mark the start of the race.
+ *   Subsequent plates will act as checkpoints.
+ * race disqualify below
+ *   This will disqualify players who fall below a certain point on the map.
+ * race end
+ *   Next plate you go over will mark the END of the race. The finish line.
+ *
+ */
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Parkour extends JavaPlugin {
+}
